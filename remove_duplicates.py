337049c01da035e5fba21832516292c4b7e359f2
Babishasankar <100859168+Babishasankar@users.noreply.github.com>
2025-09-09 10:18:56 +0530

    Add files via upload

---
+++ remove_duplicates.py
@@ -0,0 +1,9 @@
+nums=input("Enter numbers:")
+numbers=nums.split()
+unique_nums=[]
+for num in numbers:
+    if num not in unique_nums:
+        unique_nums.append(num)
+print("List without duplicates:",unique_nums)
+
+
